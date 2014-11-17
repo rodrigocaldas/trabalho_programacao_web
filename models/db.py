b46c6c95ac21e4676a2936c892f525be5d614022
rodrigocaldas <rodrigotmcaldas@comp.ufla.br>
2014-11-17 18:07:23 -0200

    No changes after this point. SQN

--- models/db.py
+++ models/db.py
@@ -72,3 +72,34 @@ db.define_table(
         label="Google Plus (https://plus.google.com/u/0/+Seu-nome)"
     ),
 )
+
+db.define_table(
+	'organizador',
+	Field('nome', length=120, notnull=True),
+	Field('foto', 'upload', requires=IS_EMPTY_OR(IS_IMAGE()) ),
+	Field(
+        'url_facebook',
+        requires=IS_EMPTY_OR(IS_URL()),
+        label="Facebook (https://facebook.com/seu-nome)",
+    ),
+    Field(
+        'url_twitter',
+        requires=IS_EMPTY_OR(IS_URL()),
+        label="Twitter (https://twitter.com/seu-nome)",
+    ),
+    Field(
+        'url_gplus',
+        requires=IS_EMPTY_OR(IS_URL()),
+        label="Google Plus (https://plus.google.com/u/0/+Seu-nome)"
+    ),
+	Field(
+        'url_Github',
+        requires=IS_EMPTY_OR(IS_URL()),
+        label="Github (https://github.com/Seu-Git)"
+    ),
+	Field(
+        'url_LinkedIn',
+        requires=IS_EMPTY_OR(IS_URL()),
+        label="LinkedIn "
+    ),
+)
