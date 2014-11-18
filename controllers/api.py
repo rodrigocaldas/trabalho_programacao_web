b591cb58732e0a57cbdf31f14289f33c0ae39d49
cassiobotaro <cassiobotaro@gmail.com>
2014-11-18 16:34:57 -0200

    @moniiica no comando! just checking if git is working properly...

--- controllers/api.py
+++ controllers/api.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 @auth.requires_login()
 def eventos():
     if request.vars['data']:
         data = request.vars['data']
-        eventos = db((db.evento.data_inicio <= data) and (db.evento.data_final >= data)).select()
+        eventos = db((db.evento.data_inicio <= data) and (
+            db.evento.data_final >= data)).select()
     else:
         eventos = db(db.evento).select()
     return eventos.as_json()
@@ -17,6 +19,16 @@ def evento():
     evento = db(db.evento.id == _id).select().first()
     if evento:
         return evento.as_json()
+    else:
+        redirect(URL('erro_404'))
+
+
+@auth.requires_login()
+def atividades():
+    _id = request.args(0) or redirect(URL('erro_404'))
+    atividades = db((db.evento.id == _id) and (db.atividade.evento_relacionado == _id) and (db.palestrante.id == db.atividade.palestrante)).select()
+    if atividades:
+        return atividades.as_json()
     else:
         redirect(URL('erro_404'))
 
