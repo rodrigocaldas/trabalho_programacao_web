f0687ccda5699af3d0ceac8f94e869c202221523
griffisu <moedacoin@gmail.com>
2014-11-18 17:13:35 -0200

    implementado organizadores fix #10

--- controllers/api.py
+++ controllers/api.py
@@ -34,6 +34,23 @@ def patrocinadores():
     else:
         redirect(URL('erro_404'))
 
+@auth.requires_login()
+def organizadores():
+    evento = request.args(0) or redirect(URL('erro_404'))
+    if evento:
+        evento = request.args(0)
+        query = db.vinculo_organizador_evento.evento == evento
+        query &= db.organizador.id==db.vinculo_organizador_evento.organizador
+        query &= db.auth_user.id==db.vinculo_organizador_evento.organizador
+        organizadores = db(query).select(db.auth_user.first_name,db.auth_user.last_name,
+                                         db.auth_user.email, db.organizador.usuario,db.organizador.foto,
+                                         db.organizador.url_facebook,db.organizador.url_twitter,
+                                         db.organizador.url_gplus,db.organizador.url_Github,
+                                         db.organizador.url_LinkedIn)
+        return organizadores.as_json()
+    else:
+        redirect(URL('erro_404'))
+
 
 @auth.requires_login()
 def atividades():
